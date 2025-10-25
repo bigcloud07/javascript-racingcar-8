dd4190923ffb1e960845c9b80e69b1ef4ab3333f
bigcloud07 <dnsxo0712@gmail.com>
2025-10-26 00:24:47 +0900

    feat: Add input functions for car names and move attempts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import carNameinput from "./InputView/carNameInput.js";
+import moveCountInput from "./InputView/moveCountInput.js";
+
 class App {
-  async run() {}
+  async run() {
+    await carNameinput();
+    await moveCountInput();
+  }
 }
 
 export default App;
---
+++ src/InputView/moveCountInput.js
@@ -0,0 +1,6 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export default async function moveCountInput() {
+    const moveCount = Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return moveCount;
+}
